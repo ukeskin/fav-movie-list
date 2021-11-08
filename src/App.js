650a6257f9e5b1edd6c78f1463a09b135e5247ba
Umut Keskin <ukeskin2626@gmail.com>
2021-11-08 13:59:19 +0300

    Add Readme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Search from "components/Search";
 import Favorite from "components/Favorite";
+
 export default function App() {
   return (
     <div className="h-screen w-full bg-gray-100 flex p-1 md:flex-row flex-col">
